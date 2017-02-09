119bdd74b029a2ed2093edc2801533a29246797c
Justin Zimmerman <zimmy130@gmail.com>
2017-02-09 00:53:42 -0500

    init client

---
+++ client/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Chart from './Chart';
+
+const App = () => (
+  <div>
+    <Chart />
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById('app'));
