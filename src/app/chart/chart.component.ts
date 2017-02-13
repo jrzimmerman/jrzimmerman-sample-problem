5c131f6f1cb3bc37b85e4901009e923df85e93e1
Justin Zimmerman <zimmy130@gmail.com>
2017-02-12 20:41:19 -0500

    add stocks service

--- src/app/chart/chart.component.ts
+++ src/app/chart/chart.component.ts
@@ -1,26 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { StockService } from '../stock.service';
 
 @Component({
   selector: 'app-chart',
+  providers: [StockService],
   templateUrl: './chart.component.html',
   styleUrls: ['./chart.component.css']
 })
 export class ChartComponent implements OnInit {
-  constructor() {
+
+  options: Object;
+  stocks: Array<any>;
+  constructor(private _stockService: StockService ) {
+    this.stocks = this._stockService.getStocks();
     this.options = {
-      title : { text : 'Closing Stock Price Over Time' },
-      series: [{
-        name: 'AAPL',
-        data: [2,3,5,8,13],
-        allowPointSelect: true
-      },{
-        name: 'IBM',
-        data: [-2,-3,-5,-8,-13],
-        allowPointSelect: true
-      }]
+      title : { text: 'Closing Stock Price Over Time' },
+      series: this.stocks,
+      xAxis: { title: { text: 'Date' } },
+      yAxis: { title: { text: 'Closing Price' } },
     };
   }
-  options: Object;
 
   ngOnInit() {
   }
--- src/app/stock.service.ts
+++ src/app/stock.service.ts
@@ -2,7 +2,21 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class StockService {
+  stocks: Array<any>;
+  constructor() {
+    this.stocks = [{
+        name: 'AAPL',
+        data: [2, 3, 5, 8, 13],
+        allowPointSelect: true
+      }, {
+        name: 'IBM',
+        data: [-2, -3, -5, -8, -13],
+        allowPointSelect: true
+      }];
+  }
 
-  constructor() { }
+  getStocks() {
+    return this.stocks;
+  }
 
 }
